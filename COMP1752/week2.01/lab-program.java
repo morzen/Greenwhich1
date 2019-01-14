7e77c193f06569510d4b6fb4ef3b5696e681d1c9
barnabe <barnabe@barnabes-MacBook-Air.local>
2019-01-14 14:31:08 +0000

    update comp1753 week 13 and comp 1752

---
+++ COMP1752/week2.01/lab-program.java
@@ -0,0 +1,22 @@
+package coursework;
+import java.util.Scanner;
+public class Coursework {
+    public static void main(String[] args) {
+
+      Scanner user_input = new Scanner(System.in);
+      String name;
+      System.out.print("Enter your name here: ");
+      name = user_input.next();
+
+
+      Scanner user_input2 =  new Scanner(System.in);
+      String age;
+      System.out.print("Enter your age here: ");
+      age = user_input.next();
+      int Age = Integer.parseInt(age);
+      System.out.println(Age);
+
+      System.out.println(name + " your age is " + age + " in days it is " + Age * 365 + " days.");
+      System.out.println(name + "knowing that the life span of a human on average is 85 year or 31025 day you have " + 31025 - (Age * 365) + " day to live , good day.");
+    }
+  }
