31c620a1c7e87a95a672dfa7443e4f464a83d6b2
barney <32000608+morzen@users.noreply.github.com>
2019-02-14 21:12:51 +0000

    Update Thu14.sql

--- COMP1713/Thu14.sql
+++ COMP1713/Thu14.sql
@@ -1,99 +1,5 @@
 
 
-
-CREATE TABLE League(
-		LeagueID INT PRIMARY KEY IDENTITY,
-		Name VARCHAR(40),
-	);
-CREATE TABLE Team(
-		TeamID INT PRIMARY KEY IDENTITY,
-		Name VARCHAR(40),
-		LeagueID INT,
-		CONSTRAINT fk_LeagueID FOREIGN KEY (LeagueID)
-					REFERENCES League(LeagueID)
-);
-CREATE TABLE Result(
-		ResultID INT PRIMARY KEY IDENTITY,
-		HomeTeamID INT,
-		AwayTeamID INT,
-		HomeScore INT,
-		AwayScore INT,
-		HomePoints INT,
-		AwayPoints INT,
-		LeagueID INT,
-		CONSTRAINT fk_HomeTeamID FOREIGN KEY (HomeTeamID)
-						 REFERENCES Team(TeamID),
-		CONSTRAINT fk_AwayTeamID FOREIGN KEY (AwayTeamID)
-						REFERENCES Team(TeamID),
-		CONSTRAINT fk_ResultLeagueID FOREIGN KEY (LeagueID)
-						REFERENCES League(LeagueID)
-);
-CREATE TABLE Player(
-    PlayerID PRIMARY KEY IDENTITY,
-    FirstName VARCHAR(20),
-    LastName VARCHAR(20),
-    DOB INT(8),
-    Sex VARCHAR,
-    CONSTRAINT fk_TeamID FOREIGN KEY (TeamID)
-                    REFERENCES Team(TeamID),
-)
-
-
-
-
-INSERT INTO League VALUES ('League One');
-INSERT INTO League VALUES ('World Cup A');
-INSERT INTO League VALUES ('World Cup B');
-
-
-
-INSERT INTO Team VALUES ('Liverpool',1);
-INSERT INTO Team VALUES ('Arsenal',1);
-INSERT INTO Team VALUES ('BarneyDaBest',1);
-INSERT INTO Team VALUES ('BrandonLoser',1);
-INSERT INTO Team VALUES ('RealBoitar',1);
-INSERT INTO Team VALUES ('BouftonRouge',1);
-INSERT INTO Team VALUES ('TofuLegs',1);
-
-SELECT * FROM Team
-
-INSERT INTO Player (FirstName,LastName) VALUES ('Ryan','Renold',2);
-INSERT INTO Player (FirstName,LastName) VALUES ('Hugh','Jackman',2);
-INSERT INTO Player (FirstName,LastName) VALUES ('Tom','Cruise',2);
-
-INSERT INTO Team (Name,LeagueID) VALUES ('Liverpool',1);
-
-
-INSERT INTO result VALUES (1,2,4,0,3,0,1);
-
-SELECT * FROM Result,
-
-SELECT * FROM team WHERE leagueID = 1;
-SELECT * FROM team WHERE leagueID = 2;
-
-SELECT Name FROM team WHERE leagueID = 2;
-SELECT FirstName FROM Player WHERE 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 INSERT INTO League VALUES ('League One');
 INSERT INTO League VALUES ('World Cup A');
 INSERT INTO League VALUES ('World Cup B');
@@ -135,4 +41,58 @@ SELECT * FROM Player;
 SELECT Name FROM team WHERE leagueID = 2;
 
 SELECT FirstName+' '+LastName FROM Player;
-ALTER TABLE Player WHERE ('(No column name)') 
+
+
+
+SELECT Team.Name, League.Name FROM team INNER JOIN 
+League ON Team.LeagueID = League.LeagueID 
+WHERE Team.LeagueID=1;
+
+ 
+SELECT COUNT(ResultID) AS HomeGamesPlayed, HomeTeamID FROM Result 
+GROUP BY HomeTeamID;
+
+SELECT SUM(HomePoints) AS "Home Points", HomeTeamID  FROM Result 
+GROUP BY HomeTeamID;
+
+
+/* Example 1: COUNT home games played and join the result to Team to show the Team.Name  */ 
+
+SELECT COUNT(ResultID) AS HomeGamesPlayed, Team.Name
+FROM Result INNER JOIN Team ON Team.TeamID=Result.HomeTeamID 
+GROUP BY Team.Name;
+
+/* Example 2: SUM home games points and join the result to Team to show the Team name */ 
+
+SELECT SUM(HomePoints) AS "Home Points", Team.Name  
+FROM Result INNER JOIN Team ON Team.TeamID=Result.HomeTeamID 
+GROUP BY Team.Name;
+
+/* Example 3: SUM home and away points and join the result to Team to show the Team name, note: the use of HomeTeamID OR AwayTeamID */ 
+
+SELECT SUM(HomePoints) AS "Home Points", SUM(AwayPoints) as "Away Points", 
+	Team.Name  
+FROM Result INNER JOIN Team ON 
+Team.TeamID=Result.HomeTeamID OR Team.TeamID=Result.AwayTeamID 
+GROUP BY Team.Name;
+
+
+SELECT (PointsResultSet.HomePoints + PointsResultSet.AwayPoints) as Points,
+ PointsResultSet.Name from
+(SELECT SUM(HomePoints) AS "HomePoints", SUM(AwayPoints) as
+"AwayPoints", Team.Name  
+FROM Result INNER JOIN Team ON Team.TeamID=Result.HomeTeamID OR Team.TeamID=Result.AwayTeamID 
+GROUP BY Team.Name) as PointsResultSet;
+
+
+SELECT (PointsResultSet.HomePoints + PointsResultSet.AwayPoints) as Points,
+ PointsResultSet.Name from
+(SELECT SUM(HomePoints) AS "HomePoints", SUM(AwayPoints) as
+"AwayPoints", Team.Name  
+FROM Result INNER JOIN Team ON Team.TeamID=Result.HomeTeamID OR Team.TeamID=Result.AwayTeamID 
+GROUP BY Team.Name) as PointsResultSet ORDER BY Points DESC;
+
+UPDATE Team SET Name='Liverpool FC' WHERE Name='Liverpool'
+
+UPDATE Team SET Name='Liverpool FC' WHERE TeamID=1
+
